577f4b35d735fe8d38779bd1cb0373602ec675ee
jrkinouani <kinouani@gmail.com>
2020-08-19 22:44:05 +0400

    add params model city

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,8 @@ class City < ApplicationRecord
 
   def self.search(search)
      where("name LIKE ?", "%#{search}%")
+     where("region LIKE ?", "%#{search}%")
+     where("place LIKE ?", "%#{search}%")
   end
+
 end

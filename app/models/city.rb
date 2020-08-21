09d5de96b02043638a84f5e5fb60822819a377f8
jrkinouani <kinouani@gmail.com>
2020-08-21 14:57:04 +0400

    add card p home

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,9 +1,8 @@
 class City < ApplicationRecord
 
- mount_uploader :image, ImageUploader 
+ mount_uploader :image, ImageUploader
 
   def self.search(search)
      where("name LIKE ?", "%#{search}%")
   end
-
 end
